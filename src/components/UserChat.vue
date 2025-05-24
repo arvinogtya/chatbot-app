9eb5f02cff19462570e11d698b9be22d1c3bdbe2
arvinogatya <arvinogatya@gmail.com>
2025-05-24 10:40:06 +0700

    chatbot

--- src/components/UserChat.vue
+++ src/components/UserChat.vue
@@ -3,7 +3,7 @@
 <template>
   <div class="w-full flex justify-end h-fit mb-2">
     <p
-      class="mr-2 w-fit bg-white dark:bg-white p-4 rounded-l-xl rounded-tr-xl border border-slate-500 whitespace-pre-line text-stone-900"
+      class="mr-2 w-fit bg-black dark:bg-black p-4 rounded-l-xl rounded-tr-xl border border-slate-500 whitespace-pre-line text-white"
     >
       <slot />
     </p>
