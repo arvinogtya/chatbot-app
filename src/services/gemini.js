5a7df5a205846ab202c034119229f35e147d5451
arvinogatya <arvinogatya@gmail.com>
2025-05-24 10:03:06 +0700

    chatbot

--- src/services/gemini.js
+++ src/services/gemini.js
@@ -1,11 +1,11 @@
 import { GoogleGenerativeAI } from '@google/generative-ai'
-const GEMINI_API = import.meta.env.VITE_GEMINI_KEY
+const GEMINI_API = import.meta.env.VITE_GEMINI
 
 const ai = new GoogleGenerativeAI({ apiKey: GEMINI_API })
 
 export const requestGemini = async (contents) => {
   const response = await ai.models.generateContent({
-    model: 'gemini-5.0-flash',
+    model: 'gemini-2.0-flash',
     contents,
   })
   return response.text
